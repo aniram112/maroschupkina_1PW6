358fe346a77f9c33e4d9a9cfc0d8cd4f24df203f
aniram112 <masyakmarina@gmail.com>
2021-11-18 15:41:29 +0300

    pod

---
+++ maroschupkina_1PW6/Pods/Target Support Files/Pods-maroschupkina_1PW6/Pods-maroschupkina_1PW6-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_maroschupkina_1PW6 : NSObject
+@end
+@implementation PodsDummy_Pods_maroschupkina_1PW6
+@end
--- maroschupkina_1PW6/maroschupkina_1PW6/ViewController.swift
+++ maroschupkina_1PW6/maroschupkina_1PW6/ViewController.swift
@@ -7,22 +7,32 @@
 
 import UIKit
 import MyLogger1
+import MyLogger2
+import MyLogger3
 
 class ViewController: UIViewController {
     @IBOutlet weak var frameworkButton: UIButton!
     @IBOutlet weak var packageButton: UIButton!
     @IBOutlet weak var podButton: UIButton!
-    
     @IBOutlet weak var carthageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         frameworkButton.addTarget(self, action: #selector(frameworkClick(sender:)), for: .touchDown)
-        // Do any additional setup after loading the view.
+        packageButton.addTarget(self, action: #selector(packageClick(sender:)), for: .touchDown)
+        podButton.addTarget(self, action: #selector(podClick(sender:)), for: .touchDown)
+    
     }
     @objc func frameworkClick(sender: Any) {
-            MyLogger1.log("kachow")
-        }
-
-
+        MyLogger1.log("kachow")
+    }
+    @objc func packageClick(sender: Any) {
+        MyLogger2.log("yeet")
+    }
+    @objc func podClick(sender: Any) {
+        MyLogger3.log("bruh")
+    }
+    
+    
 }
 
